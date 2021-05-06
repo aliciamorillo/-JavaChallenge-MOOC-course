fa5422e57078a34f40223859a6a36ce5bf402ba9
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-05-06 23:20:59 +0200

    Add files via upload

---
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class NameOfTheOldest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int age = 0;
+        int oldest = 0;
+        String name = null;
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] pieces = input.split(",");
+            age = Integer.valueOf(pieces[1]);
+            
+
+            if (age > oldest) {
+                oldest = age;
+                name = pieces[0];
+            }
+        }
+        
+        System.out.println("Name of the oldest: " + name);
+    }
+}
