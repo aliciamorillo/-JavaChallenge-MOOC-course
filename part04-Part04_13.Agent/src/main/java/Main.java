8de477e348b4c434c5d21bc26b3a058bf4ae29e1
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-05-09 12:48:16 +0200

    Add files via upload

---
+++ part04-Part04_13.Agent/src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Agent agent = new Agent("Nolan", "the Dog");
+        
+        System.out.println(agent);
+    }
+    
+}
