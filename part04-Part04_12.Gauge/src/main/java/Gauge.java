132d8fe7d53a3d5056b3794cd51ea2c9ba7e3d3f
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-05-09 12:48:04 +0200

    Add files via upload

---
+++ part04-Part04_12.Gauge/src/main/java/Gauge.java
@@ -0,0 +1,33 @@
+
+public class Gauge {
+    
+    private int value;
+    
+    public Gauge(){
+        this.value = 0;
+    }
+    
+    public void increase(){
+        if (this.value < 5) {
+            value++;
+        }
+    }
+    
+    public void decrease(){
+        if (this.value > 0) {
+            value--;
+        }
+    }
+    
+    public int value(){
+        return this.value;
+    }
+    
+    public boolean full(){
+        if(value == 5){
+            return true;
+        }
+        return false;
+    }
+    
+}
