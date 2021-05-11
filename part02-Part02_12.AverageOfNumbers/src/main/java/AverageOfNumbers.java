180fbad32a18661035b0a655cbac14e80796dd99
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-05-11 08:49:45 +0200

    Add files via upload

---
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        double sum = 0;
+        int count = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if(input == 0){
+                break;
+            }
+            
+            count++;
+            sum += input;
+        }
+        
+        System.out.println("Average of the numbers: " + (sum/count));
+    }
+}
