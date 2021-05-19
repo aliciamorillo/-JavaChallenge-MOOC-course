bda1c608dcf62f53c60bc0a49a2e1331254567c2
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-05-19 10:35:28 +0200

    Add files via upload

---
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class LineByLine {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String line = scanner.nextLine();
+            
+            if (line.equals("")) {
+                break;
+            }
+
+            String[] pieces = line.split(" ");
+
+            for (String piece : pieces) {
+                System.out.println(piece);
+            }
+        }
+        
+
+    }
+}
