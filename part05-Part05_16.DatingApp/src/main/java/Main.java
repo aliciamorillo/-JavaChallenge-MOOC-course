8669123f007bb084909e42001396ecac9a54c3ae
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-05-24 08:55:41 +0200

    Add files via upload

---
+++ part05-Part05_16.DatingApp/src/main/java/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        SimpleDate now = new SimpleDate(13, 2, 2015);
+        SimpleDate afterOneWeek = now;
+        afterOneWeek.advance(7);
+
+        System.out.println("Now: " + now);
+        System.out.println("After one week: " + afterOneWeek);
+        
+        SimpleDate date = new SimpleDate(13, 2, 2015);
+
+        SimpleDate newDate = date.afterNumberOfDays(7);
+        int week = 1;
+        while (week <= 7) {
+        System.out.println("Friday after " + week + " weeks is " + newDate);
+        newDate = newDate.afterNumberOfDays(7);
+
+        week = week + 1;
+    }
+    }
+}
